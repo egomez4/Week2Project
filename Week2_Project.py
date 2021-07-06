685799872a7ffa120a66b9c5108e9405b4922c53
egomez4 <egomez4@uncc.edu>
2021-07-06 19:00:37 +0000

    updated code, we have a basic working program.
    Co-authored-by: Jordy Reyes <jordyreyes8@gmail.com>

--- Week2_Project.py
+++ Week2_Project.py
@@ -13,14 +13,32 @@ def song_search(song_artist, song_title):
 
     track_id = song['message']['body']['track_list'][0]['track']['track_id']
     commontrack_id = song['message']['body']['track_list'][0]['track']['commontrack_id']
-    
-    return track_id
+    return (track_id)
 
 
-def get_lyrics(track_id):    
+def get_lyrics(track_id):  
+    key = "d01d05bd4c4b77d9143f319216a6342b"
     lyrics_url = f'http://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey={key}&track_id={track_id}'
     lyrics = requests.get(lyrics_url)
     lyrics_data = lyrics.json()
-    lyrics_body = lyrics_data['message']['body']['lyrics']['lyrics_body']
+    lyrics_body = str(lyrics_data['message']['body']['lyrics']['lyrics_body'])
+    
     return (lyrics_body)
 
+def sentiment (lyrics):
+    sentiment_url = 'http://text-processing.com/api/sentiment/'
+    options = { 'text' : lyrics}
+    response = requests.post(sentiment_url, data = options)
+    sentiment = response.json()
+    sentiment
+    return sentiment
+
+
+try:
+    song_id = song_search(song_artist, song_title)
+    lyrics = get_lyrics(song_id)
+    song_analysis = sentiment(lyrics)
+    print(song_analysis)
+
+except TypeError as e:
+    print('Error: Lyrics are Unavailable')
